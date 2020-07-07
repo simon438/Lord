b1bb164faa3d1cc0393fb9fb2aee79e1e61ae43f
simon438 <simonstephenk99@gmail.com>
2020-07-07 15:08:53 +0000

    Verifying for clarity

---
+++ Refer.cmd
@@ -0,0 +1,3 @@
+Hello Worls I am waiting your reply 
+When you are coming to take a task
+Hurry in the desk
---
+++ hyphen.java
@@ -0,0 +1,3 @@
+ There is nothing in the worls that love cant conquer
+ Love your neighbour as yourself
+ Keeping in mind is very difficult
